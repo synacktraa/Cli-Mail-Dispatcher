78a4b40be51a9d00cb9783d16dc83e4acc970aea
Mikey <harshmetaexploit90@gmail.com>
2022-03-06 13:47:35 +0530

    fixed terminal bug

--- models/graphics.py
+++ models/graphics.py
@@ -3,23 +3,36 @@ import sys
 from models.wash import cstdout
 
 
-def weirdShit(): # function for some graphics shit
+def weirdShit(): 
 
     def spinning_cursor():
         while True:
             for cursor in '|/-\\':
-             yield cursor
+                yield cursor
                   
     spinner = spinning_cursor()
 
-    lowerstr = "[^] serving metadata"
-    upperstr = lowerstr.upper()
+    data = "[+] serving metadata"
+    upperstr = data.upper()
+    data_len = len(data)
+    spaces = str().expandtabs(8)
+
+    while data_len:
+        if(data_len<8):
+            spaces += '\t'
+            for _ in range(8-data_len):
+                spaces += '\b'
+            data_len = 0
+        else:
+            spaces += '\t'
+            data_len -= 8
+
     for _ in range(3):
-        for x in range(len(lowerstr)):
-            s = '\r' + lowerstr[0:x] + upperstr[x] + lowerstr[x+1:] + '\r'
+        for x in range(len(data)):
+            s = '\r' + data[0:x] + upperstr[x] + data[x+1:] + '\r'
             cstdout(s, "yellow")
-            cstdout("\t\t\t\b\b\b\b...", "lightwhite")
-            cstdout(next(spinner),"lightwhite") 
+            cstdout(f"{spaces}...", "lightwhite")
+            cstdout(next(spinner), "lightwhite") 
             cstdout("\r")
             sys.stdout.flush()
             time.sleep(0.1/0.8)
